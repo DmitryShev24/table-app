18e8dbf918895af34399afa87f927985a3933b32
Shevchenko Dmitry <dmitriy.e.shevchenko@rt.ru>
2021-06-28 17:17:05 +0300

    первая домашка с 1 и 2 заданием

---
+++ src/HomeWork.js
@@ -0,0 +1,43 @@
+// Ребят, как и обещал - интересное ДЗ:
+// ++ 1. сделать инпут с кнопкой - при нажатии на кнопку у нас заменяется тайтл вкладки на текст из инпута, причём он там как бегущая строка - вспоминаем билборды заманухи.
+// 2. Написать функцию которая принимает массив чисел - задача вернуть предпоследнее наивысшее чётное число.
+// 3. Продолжаем развивать нашу табличку, устанавливаем библиотеку react-dropzone, - задача при клике с задержкой на строку в таблице равную 2 секунды, мы можем перетачить строку в другое место изменив позицию - примеров в интернете вагон, ничего сложного, для вас + популярная либа в копилку.
+
+export const HomeWork = () => {
+    return two();
+}
+
+const one = () => {
+    const changeTitle = (e) => {
+        e.preventDefault();
+        let newTitle = document.forms.test.title.value;
+        document.title = newTitle;
+        setInterval(() => {
+            let lenta = document.title;
+            document.title = lenta.substr(1, lenta.length)+lenta.charAt(0);
+        }, 100);
+    }
+        return (
+            <form name="test">
+                <input type="text" name="title" size={50}/>
+                <input type="submit" value="Меняем тайтл" onClick={changeTitle}/>
+            </form>
+        )
+}
+
+const two = () => {
+    let newArray = [1,4, 234 ,6,3,2,5,]
+    let sortArray = []
+    newArray.forEach(e => {
+        let val = !(e % 2);
+        if (val) {
+            sortArray.push(e);
+        }
+    })
+    sortArray.sort((a,b) => {
+        return a-b
+    } );
+    return (
+        <div>{sortArray[sortArray.length-2]}</div>
+    )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {HomeWork} from './HomeWork';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  //   {/* <App /> */}
+
+  // </React.StrictMode>,
+  <div>
+    {HomeWork()}
+  </div>,
   document.getElementById('root')
 );
 
